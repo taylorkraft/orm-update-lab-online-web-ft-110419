44a660854b0bd02683915a483a6f56ca9e196a2c
Taylor Kraft <taylorkraft16@gmail.com>
2019-12-27 01:40:24 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,11 +4,21 @@ class Student
   attr_accessor :name, :grade
   attr_reader :id
 
-def initialize(id = nil, name, grade)
-  @id = id
-  @name = name
-  @grade = grade
-end
+  def initialize(id = nil, name, grade)
+    @id = id
+    @name = name
+    @grade = grade
+  end
 
+  def self.create_table
+    sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS students (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade INTEGER
+    )
+    SQL
+    DB[:conn].execute(sql)
+  end
 
 end
