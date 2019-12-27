fbd4ef0873fb95536a5f586696731660aebeab70
Taylor Kraft <taylorkraft16@gmail.com>
2019-12-27 02:01:51 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -54,4 +54,10 @@ class Student
     new_student.grade = row[2]
     new_student
   end
+
+  def self.find_by_name(name)
+    sql = "SELECT * FROM students WHERE name = ?"
+    result = DB[:conn].execute(sql, name)[0]
+    Student.new(result[0], result[1], result[2])
+  end
 end
