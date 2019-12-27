f83ad422bad839cf53566e0e59189a2ce1c77dfd
Taylor Kraft <taylorkraft16@gmail.com>
2019-12-27 02:05:01 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -60,4 +60,9 @@ class Student
     result = DB[:conn].execute(sql, name)[0]
     Student.new(result[0], result[1], result[2])
   end
+
+  def update
+    sql = UPDATE students SET name = ?, grade = ?, id = ?
+    DB[:conn].execute(sql)
+  end
 end
