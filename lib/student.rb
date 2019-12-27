a2ef8e9e32889155437e468023f6b35593cb011b
Taylor Kraft <taylorkraft16@gmail.com>
2019-12-27 01:56:48 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -46,4 +46,12 @@ class Student
     new_student.save
     new_student
   end
+
+  def self.new_from_db(row)
+    new_student = self.new
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2]
+    new_student
+  end
 end
